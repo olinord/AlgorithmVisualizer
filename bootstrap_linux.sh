ef3f4becf573aaddae24764ed82e123414f0d1ea
olinord <olinord@gmail.com>
2014-08-31 20:32:15 +0000

    Fixed a pip install that was wrong

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -5,4 +5,4 @@ source python_virtualenv/bin/activate
 pip install euclid==0.01
 pip install pyOpengl==3.1.0
 pip install pySide==1.2.2
-pip install yaml
+pip install pyYaml==3.11
--- bootstrap_win.bat
+++ bootstrap_win.bat
@@ -2,7 +2,7 @@ call virtualenv python_virtualenv
 
 call python_virtualenv/Scripts/activate.bat
 
-call pip install euclid
-call pip install pyOpengl
-call pip install pySide
-call pip install yaml
+call pip install euclid==0.01
+call pip install pyOpengl==3.1.0
+call pip install pySide==1.2.2
+call pip install pyYaml==3.11
